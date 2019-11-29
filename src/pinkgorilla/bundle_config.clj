26085e2e06aaa4f7eeeff1c668f6e3238104e5b7
awb99 <andreas.wolfgang.bauer@gmail.com>
2019-11-28 21:56:24 -0500

    more bundle deps

--- src/pinkgorilla/bundle_config.clj
+++ src/pinkgorilla/bundle_config.clj
@@ -11,9 +11,19 @@
             ['awb99/shapes "0.1.2"] ; shape library borrowed from maria-cloud
             ['org.pinkgorilla/gorilla-renderable "2.1.2"]
             ['quil "3.1.0"] ;drawing library
+            ['reagent "0.8.1"]
             ]
     :npm {"moment" "^2.24.0"
           "p5" "^0.9.0"  ; p5 is a quil cljsjs dependency, v 0.10.x has an error and cannot be used
+          "react" "^16.12.0"
+          "react-dom" "^16.0.0"
+          "create-react-class" "^15.6.3"
+          "bulma-extensions" "^6.2.7"
+          "bulma-tooltip" "^2.0.2"
+          "path" "^0.12.7"
+          "react-flip-move" "^3.0.3"
+          "react-highlight.js" "^1.0.7"
+          "shake.js" "^1.2.2"
           }
     :ns ['clojure.string
          'clojure.pprint
@@ -21,11 +31,14 @@
          'awb99.shapes.core
          'pinkgorilla.ui.gorilla-renderable
          ;'pinkgorilla.ui - this brings up namespace not available error.
-         'quil.sketch
-         'quil.middleware
+         ;'quil.sketch
+        ; 'quil.middleware
          'quil.core
+        ; 'quil.util
          "moment"
-         ;"p5"
+         "p5"
+         "react"
+         'reagent.core
          ]}
    :mariacloud
    {:maven [['awb99.fortune "0.0.2"]
@@ -38,7 +51,8 @@
            "codemirror" "^5.30.0"
            "react" "^16.0.0"
            "react-dom" "^16.0.0"
-           "shadow-cljs" "^2.0.41"}
+           "shadow-cljs" "^2.8.76"
+           "moment" "^2.24.0"}
     :ns ['fortune.core
          'cells.cell
          'cells.lib
@@ -48,7 +62,7 @@
          'lark.value-viewer.core
          're-db.d
          're-db.patterns
-
+         "moment"
          ;'thi.ng.geom.svg.core
          ]}})
 
