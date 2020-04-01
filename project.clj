5519b4e3faa2b5f1f8bac4c61902c97be937ae65
CI/CD <ci@pinkgorilla.org>
2020-04-01 10:04:21 +0000

    Release 0.0.11

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.pinkgorilla/kernel-cljs-shadowdeps "0.0.11-SNAPSHOT"
+(defproject org.pinkgorilla/kernel-cljs-shadowdeps "0.0.11"
   :description "cljs bundle generator that works with shadow-cljs,
                 so that self hosted clojurescript can load additional dependencies"
   :url "https://github.com/pink-gorilla/kernel-cljs-shadowdeps"
